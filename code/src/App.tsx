487125684f90cf07307e5f203fc82cee6787d0e8
yashpola <yashwit.po@gmail.com>
2024-06-04 20:02:22 +0800

    Test supabase connection

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const postData = () => {
+    fetch("http://localhost:3001/", {
+      method: "POST",
+      body: "8659c132-d4ae-4ddb-8a57-b38c0fc25c8a",
+      headers: {
+        "Content-type": "text/plain",
+      },
+    })
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => console.log(data));
+  };
+  return <button onClick={postData}>post</button>;
 }
 
 export default App;
